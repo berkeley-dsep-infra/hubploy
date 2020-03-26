63c668d0c4a074ed0bd2df265cda4dfdfcd2840b
YuviPanda <yuvipanda@gmail.com>
2020-03-26 19:00:28 +0530

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hubploy',
-    version='0.1.0',
+    version='0.1.1',
     url="https://github.com/yuvipanda/hubploy",
     author="Yuvi Panda",
     packages=setuptools.find_packages(),
