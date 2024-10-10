4489c5a60483d840a3eeaea9677452d657e94833
shane knapp <incomplete@gmail.com>
2024-10-10 13:15:50 -0700

    adding min python version req for hubploy

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     author="Shane Knapp",
     packages=setuptools.find_packages(),
     install_requires=["kubernetes", "boto3"],
+    python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             "hubploy = hubploy.__main__:main",
